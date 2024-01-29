5af96811a7e87dc6ac57f8ecf8739bd83db3ddbd
Malik Kurt <malikkurt612@gmail.com>
2024-01-29 18:27:19 +0300

    Add project files.

---
+++ WCom/loginScreenForm.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WCom
+{
+    public partial class loginScreenForm : Form
+    {
+        private Button button1;
+
+        public loginScreenForm()
+        {
+            InitializeComponent();
+        }
+
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(186, 317);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // loginScreenForm
+            // 
+            this.ClientSize = new System.Drawing.Size(458, 375);
+            this.Controls.Add(this.button1);
+            this.Name = "loginScreenForm";
+            this.ResumeLayout(false);
+
+        }
+    }
+}
